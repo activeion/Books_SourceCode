cb022395d1c0be7837c1b31296647b4fde051cb2
activeion <ji.zhonghua@qq.com>
2017-08-22 20:53:02 +0800

    improve code.cc

--- Item05/code.cc
+++ Item05/code.cc
@@ -1,22 +1,28 @@
+#include <memory>
 
 template<typename It>
 void dwim(It b, It e)
 {
     while(b != e){
-        typename std::iterator_traits<It>::value_type
-            currValue = *b;
+        //typename std::iterator_traits<It>::value_type currValue = *b;
+        auto currValue = *b;
         //...
     }
 }
 
-template<typename It>   //和之前一样
-void dwim(It b, It e)
+class Widget 
 {
-    while(b != e){
-        auto currValue =*b;
-        //...
+    public:
+        Widget(int value): value_(value){}
+    bool operator < (const Widget& w) 
+    {
+        if(value_ < w.value_) return true;
+        else return false;
     }
-}
+    private: 
+        int value_;
+};
+
 
 int main(void)
 {
@@ -24,13 +30,13 @@ int main(void)
     //auto x2;        //错误！需要初始化表达式
     auto x3 = 0;    //好的，x的值是良好定义的
 
-    auto derefUPLess =
-            [](const std::unique_ptr<Widget>& p1,
-                           const std::unique_ptr<Widget>& p2)
-                { return *p1 < *p2; };
+    auto derefUPLess = [](const std::unique_ptr<Widget>& p1, const std::unique_ptr<Widget>& p2) { return *p1 < *p2; };
+    auto derefLess = [](const auto& p1, const auto& p2) { return *p1 < *p2; };
 
-    auto derefLess =
-            [](const auto& p1,
-                          (const auto& p2)
-                              { return *p1 < *p2; };
+    std::unique_ptr<Widget> p1(new Widget(5));
+    std::unique_ptr<Widget> p2(new Widget(65));
+
+    bool b1 = derefUPLess(p1, p2);
+    bool b2 = derefLess(p1, p2);
+
 }
