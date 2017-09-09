b2d3467d564eafb1c41f7b92716403bfc79f04b3
activeion <ji.zhonghua@qq.com>
2017-09-09 23:40:13 +0800

    improve Item14

--- Item14-noexcept/code.cc
+++ Item14-noexcept/code.cc
@@ -1,16 +1,14 @@
+#include <iostream>
+
 int f(int x) throw();           //C++98风格
 int f(int x) noexcept;          //C++11风格
 
-void setup(){}
-void cleanup(){}
-
-void setup();       //做别处定义的函数, except
-void cleanup();
-
+void setup(){} //non-noexcept函数
+void cleanup(){} //non-noexcept函数
 
 
 //强制声明为 noexcept, why????
-//因为我们确定setup()/cleanup()都没有异常抛出，但他们都没有声明为noexcept
+//因为我们确定setup()/cleanup()都没有异常抛出，虽然它们没有声明noexcept
 //因为setup()、cleanup()可能是没有c++11异常支持的函数: 
 //1. C语言写的函数,比如std::strlen() 
 //2. C++98库的函数,目前还没有完全修改过来。
@@ -21,12 +19,40 @@ void dowork() noexcept
     cleanup();//清理工作
 }
 
+/*****
+ * 当使用noexcept时，std::teminate()函数会被立即调用，而不是调用std::unexpected(); 
+ * 因此，在异常处理的过程中，编译器不会回退栈，这为编译器的优化提供了更大的空间
+ */
+
+//double division(int a, int b)
+double division(int a, int b) noexcept
+{
+    if( b == 0 ) {
+        throw "Division by zero condition!"; //const char* 类型的异常
+    }
+    return (a/b);
+}
+
+
 int main(void)
 {
-    //操作f()有noexcept则move, 没有则copy
+    {
+        //操作f()有noexcept则move, 没有则copy
 
-    dowork();
+        dowork();
+    }
 
+    {
+        int x = 50;
+        int y = 0;
+        double z = 0;
+        try {
+            z = division(x, y);
+            std::cout << z << std::endl;
+        } catch (const char* msg) { //抓住const char*类型的异常
+            std::cerr << msg << std::endl;
+        }
+    }
 
     return 0;
 }
