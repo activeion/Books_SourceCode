41fbc4a0cd5c2d52071f6273691e3a20939be154
activeion <ji.zhonghua@qq.com>
2017-09-10 16:54:09 +0800

    improve Item21

--- Item21-make_shared_make_uniqure/code.cc
+++ Item21-make_shared_make_uniqure/code.cc
@@ -16,33 +16,39 @@ int computePriority(void)
 int main(void)
 {
     {
-        auto upw1(std::make_unique<Widget>());      //使用make函数
-        auto spw1(std::make_shared<Widget>());      //使用make函数
+        //使用make函数, upw1持有一个堆中的Widget对象
+        auto upw1(std::make_unique<Widget>());      
+        //使用make函数, spw1持有一个堆中的Widget对象
+        auto spw1(std::make_shared<Widget>());
 
-        std::unique_ptr<Widget> upw2(new Widget);   //不使用make函数
-        std::shared_ptr<Widget> spw2(new Widget);   //不使用make函数
+        //不使用make函数
+        std::unique_ptr<Widget> upw2(new Widget);   
+        //不使用make函数
+        std::shared_ptr<Widget> spw2(new Widget);
+    }//自动销毁资源
+
+    {
+        processWidget(std::shared_ptr<Widget>(new Widget),//潜在的资源泄露 
+                computePriority());//函数返回前资源销毁
+        processWidget(std::make_shared<Widget>(),//没有资源泄露
+                computePriority());//函数返回前资源销毁
     }
 
     {
-        processWidget(std::shared_ptr<Widget>(new Widget),  //潜在的资源泄露 
-                computePriority());
-        processWidget(std::make_shared<Widget>(),       //没有资源泄露
-                computePriority());   
-    }
-
-    {
-        std::shared_ptr<Widget> spw(new Widget); //两次内存分配: 一次new; 一次cb
-        auto spw2 = std::make_shared<Widget>();//一次内存分配: new Widget和cb一次就行，而且放在一起.
-    }
+        //两次内存分配: 一次new; 一次cb
+        std::shared_ptr<Widget> spw(new Widget);
+        //一次内存分配: new Widget和cb一次就行，而且放在一起 
+        auto spw2 = std::make_shared<Widget>();
+    }//自动销毁资源
 
     { // 不得不使用new --- 定制deleter
         auto widgetDeleter = [](Widget* pw) {;}; 
         //直接使用new来创建一个智能指针：
         std::unique_ptr<Widget, decltype(widgetDeleter)> upw(new Widget, widgetDeleter);
         std::shared_ptr<Widget> spw(new Widget, widgetDeleter);
-    }
+    }//自动销毁资源
 
-    {
+    {// 向make函数传递参数
         auto upv = std::make_unique<std::vector<int>>(10, 20);
         auto spv = std::make_shared<std::vector<int>>(10, 20);
 
@@ -54,8 +60,8 @@ int main(void)
 
     {
         std::shared_ptr<Widget> spw(new Widget);
-        processWidget(spw, computePriority());              //对的，但是不是最优的，看下面
-        processWidget(std::move(spw), computePriority());   //性能和异常安全都有保证,move不需要维护引用计数,太棒了
+        processWidget(spw, computePriority());              //对的，但是不是最优的，看下面一句
+        processWidget(std::move(spw), computePriority());   //性能和异常安全都有保证,move不需要维护引用计数,即move前后引用计数无变化,太棒了
     }
 
     return 0;
