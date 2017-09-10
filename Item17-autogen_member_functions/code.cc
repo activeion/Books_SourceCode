20df4c1d1d0e70a3be82ff33a79222ea2431fa6e
activeion <ji.zhonghua@qq.com>
2017-09-10 13:23:45 +0800

    improve Item17

--- Item17-autogen_member_functions/code.cc
+++ Item17-autogen_member_functions/code.cc
@@ -1,75 +1,110 @@
-ass Widget{
-    public:
-        //...
-            Widget(Widget&& rhs);               //move构造函数
+#include <string>
+#include <map>
 
-        Widget& operator=(Widget&& rhs);    //move assignment operator
+class Widget
+{
+    public:
+        Widget(){}                              //自己写了move构造函数，必须自己写默认构造函数
+        Widget(Widget&& rhs)                    //move构造函数
+        {
+            name_ = rhs.name_;
+        }
+        Widget& operator=(Widget&& rhs)         //move assignment operator
+        {
+            name_ = rhs.name_;
+            return (*this);
+        }
         //...
+    private:
+        std::string  name_;
 };
 
-class Widget {
+class Widget2 
+{
     public:
         //...
-            ~Widget();                  //user-declared析构函数
-
+        Widget2(){}                             //自己写了copy构造函数，必须自己写默认构造函数
         //...
-            Widget(const Widget&) = default;    //默认的拷贝构造函数的行为OK的话
-
-        Widget&
-            operator=(const Widegt&) = default; //默认的行为OK的话
+        Widget2(const Widget2&) = default;      //默认的拷贝构造函数的行为OK的话
+        Widget2& operator=(const Widget2&) = default; //默认的行为OK的话
+        ~Widget2(){}                             //user-declared析构函数
         //...
+    private: 
+        std::string name_;
 };
 
-
-class Base{
+class Base
+{
     public:
-        virtual ~Base() = default;              //让析构函数成为virtual
 
+        Base(){};                               // 使用了default也算是自定义了特殊函数, 因此必须自定义默认构造函数.
+
+        Base(const Base&) = default;            //支持copy
+        Base& operator=(const Base&) =default;
         Base(Base&&) = default;                 //支持move
         Base& operator=(Base&) = default;   
 
-        Base(const Base&) = default;            //支持copy
-        Base& operator=(const Base*) = default;
-
+        virtual ~Base() = default;              //让析构函数成为virtual
         //...
+
+    private:
+        std::string name_;
 };
 
 
-class StringTable{
+class StringTable
+{
     public:
         StringTable() {}
-        //...                     //插入，删除，查找函数等等，但是没有
-            //copy/move/析构函数
+        //...                                   //插入，删除，查找函数等等，但是没有copy/move/析构函数
 
     private:
-            std::map<int, std::string> values;
+        std::map<int, std::string> values;
 };
 
+void makeLogEntry(const char * str) {};
 
-class StringTable{
+class StringTable2
+{
     public:
-        StringTable() 
-        { makeLogEntry("Creating StringTable object");}     //后加的
-
-        ~StringTable()
-        { makeLogEntry("Destroying StringTable object");}   //也是后加的
-
-        //...                                                 //其他的函数
-
+        StringTable2() 
+        { 
+            makeLogEntry("Creating StringTable object");//后加的
+        }     
+        ~StringTable2()
+        { 
+            makeLogEntry("Destroying StringTable object");//也是后加的
+        }   
+        //... //其他的函数
     private:
-            std::map<int, std::string> values;
+        std::map<int, std::string> values;
 };
 
-class Widget{
+class Widget3
+{
     public:
         //...
-            template<typename T>
-            Widget(const T& rhs);               //构造自任何类型
+        template<typename T>
+            Widget3(const T& rhs);               //构造自任何类型
 
         template<typename T>
-            Widget& operator=(const T& rhs);    //赋值自任何类型
+            Widget3& operator=(const T& rhs);    //赋值自任何类型
 
         //...
+    private:
+        std::string name_;
 };
+
+int main(void) 
+{
+    Widget w;
+    Widget2 w2;
+    Base b;
+    StringTable t;
+    StringTable2 t2;
+
+    return 0;
+}
+
 
 
