e05b489dc6dab90ec3f09ce8378d0b1cf0f0efc7
activeion <ji.zhonghua@qq.com>
2017-10-16 11:28:06 +0800

    improve Item40/code.cc

--- Effective_Modern_Cpp/Item40-std::atomic_volatile/code.cc
+++ Effective_Modern_Cpp/Item40-std::atomic_volatile/code.cc
@@ -6,7 +6,8 @@ std::atomic<int> ac(0);
 volatile int vc(0);
 void f(void)
 {
-    for(int i=0; i<1000; ++i) {
+    // 多次重复，ac,vc将不相等
+    for(int i=0; i<10000; ++i) {
         ++ac;
         ++vc;
     }
@@ -23,21 +24,36 @@ int main(void)
 
     {// volatile's function: forbidden code optimization
         {
-            int x; 
-            x = 10; // write x, compilers can eliminate this!
-            x = 20; // write x again
-            auto y = x;  // read x
-            y = x;      // read x again, compilers can eliminate this!
+            int x=0; 
+            auto y = x;     // read x
+            y = x;          // read x again, compilers can eliminate this!
             (void)y;
+            x = 10;         // write x, compilers can eliminate this!
+            x = 20;         // write x again
         }
         {// normally, x is a special memory, eg. register of peripherals
-            volatile int x; 
-            x = 10; // write x, compilers can NOT eliminate this!
-            x = 20; // write x again
-            auto y = x;  // read x
-            y = x;      // read x again, compilers can NOT eliminate this!
+            volatile int x; // no un-initializtion warning
+            auto y = x;     // read x
+            y = x;          // read x again, compilers can NOT eliminate this!
             (void)y;
+            // 10 behaves a command to peripherals, eg. radio transmitter
+            x = 10;         // write x, compilers can NOT eliminate this!
+            // 20 behaves another command to peripherals, eg. radio transmitter
+            x = 20;         // write x again
         }
+    }
+
+    {// std::atomic的copy构造和赋值操作符被删除, 因此根本不能代替volatile
+        std::atomic<int> x;
+        //auto y = x;           // conceptually read x, error! copy ctor deleted
+        //y = x;                // conceptually read x again, error! assign op deleted
+        x = 10;               // write x
+        x = 20;               // write x again
+
+        //多线程中，存取peripherals寄存器, 注意不是cpu寄存器
+        volatile std::atomic<int> vai;    // operations on vai are
+                                          // atomic and can't be
+                                          // optimized away
     }
 
     return 0;
