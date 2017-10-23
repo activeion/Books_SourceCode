1c3a13d4edbc951a238d1095a369a4ad96bc954b
activeion <ji.zhonghua@qq.com>
2017-10-23 14:36:55 +0800

    simplify halfsearch/halfsearch_d()

--- Fluent_Python/ch02/binsect.py
+++ Fluent_Python/ch02/binsect.py
@@ -6,72 +6,49 @@ NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
 
 ROW_FMT = '{0:2d} @ {1:2d}    {2}{0:3d}'
 
+scores = [33, 99, 77, 70, 89, 90, 100, 101, -1, 60]
+breakpoints = [60, 70, 80, 90]
 
 def halfsearch(ar, target):
     lo=0
-    hi=len(ar)-1 
+    hi=len(ar) 
     mid=0
     while(lo<hi):
         mid = (hi+lo)//2
         #print("processing..., lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
         if(target<ar[mid]): #升序,左半边
-            hi=mid-1#出界点
-        elif (target>ar[mid]): #升序,右半边
-            lo=mid+1#入界点
-        else:# target == ar[mid]
+            hi=mid          #调整出界点
+        else:               #升序,右半边
             #print("just found. lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
-            return mid+1
-    #print("nothing found. lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
-    if(target>=ar[-1]):
-        return len(ar)
-    elif(target<=ar[0]):
-        return 0
-    else:
-        return mid+1
+            lo=mid+1
+    return lo
 
 def halfsearch_left(ar, target):
     lo=0
-    hi=len(ar)-1 
+    hi=len(ar) 
     mid=0
     while(lo<hi):
         mid = (hi+lo)//2
         #print("processing..., lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
-        if(target<ar[mid]): #升序,左半边
-            hi=mid-1#出界点
-        elif (target>ar[mid]): #升序,右半边
-            lo=mid+1#入界点
-        else:# target == ar[mid]
+        if (target>ar[mid]):#升序,右半边
+            lo=mid+1        #调整入界点
+        else:               #升序,左半边
+            hi=mid
             #print("just found. lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
-            return mid
-    #print("nothing found. lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
-    if(target>ar[-1]):
-        return len(ar)
-    elif(target<=ar[0]):
-        return 0
-    else:
-        return mid+1
+    return lo
 
 def halfsearch_d(ar, target):
     lo=0
-    hi=len(ar)-1 
+    hi=len(ar) 
     mid=0
     while(lo<hi):
         mid = (hi+lo)//2
         #print("processing..., lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
         if(target>ar[mid]): #升序,左半边
-            hi=mid-1#出界点
-        elif (target<ar[mid]): #升序,右半边
+            hi=mid#出界点
+        else: #升序,右半边
             lo=mid+1#入界点
-        else:# target == ar[mid]
-            #print("just found. lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
-            return mid+1
-    #print("nothing found. lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
-    if(target>=ar[0]):
-        return 0
-    elif(target<=ar[-1]):
-        return len(ar)
-    else:
-        return mid+1
+    return lo
 
 def demo(bisect_fn):
     for needle in reversed(NEEDLES):
@@ -79,7 +56,7 @@ def demo(bisect_fn):
         offset = position*"  |"
         print(ROW_FMT.format(needle, position, offset))
 
-def grade(bisect_fn, score, breakpoints =[60, 70, 80, 90], grades='FDCBA'):
+def grade(bisect_fn, score, breakpoints , grades='FDCBA'):
     i = bisect_fn(breakpoints, score)
     return grades[i]
 
@@ -92,6 +69,7 @@ if __name__ == '__main__':
         bisect_fn = halfsearch
     elif sys.argv[-1] == 'halfsearch_d':
         HAYSTACK =[x for x in reversed(HAYSTACK)]
+        breakpoints = [x for x in reversed(breakpoints)]
         bisect_fn = halfsearch_d
     else:
         bisect_fn = bisect.bisect
@@ -101,10 +79,8 @@ if __name__ == '__main__':
     print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
     demo(bisect_fn)
 
-
-    scores = [33, 99, 77, 70, 89, 90, 100, 101, -1, 60]
     print(scores)
-    print([grade(bisect_fn,score) for score in scores]) 
+    print([grade(bisect_fn,score,breakpoints) for score in scores]) 
 
 
 
