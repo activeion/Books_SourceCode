01bc4435b4fc587dcc235c6a2c6c61a879081f92
activeion <ji.zhonghua@qq.com>
2017-10-23 11:08:28 +0800

    add ch05

--- Fluent_Python/ch02/binsect.py
+++ Fluent_Python/ch02/binsect.py
@@ -29,6 +29,28 @@ def halfsearch(ar, target):
     else:
         return mid+1
 
+def halfsearch_left(ar, target):
+    lo=0
+    hi=len(ar)-1 
+    mid=0
+    while(lo<hi):
+        mid = (hi+lo)//2
+        #print("processing..., lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
+        if(target<ar[mid]): #升序,左半边
+            hi=mid-1#出界点
+        elif (target>ar[mid]): #升序,右半边
+            lo=mid+1#入界点
+        else:# target == ar[mid]
+            #print("just found. lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
+            return mid
+    #print("nothing found. lo=%d,mid=%d,hi=%d,ar[mid]=%d\n" %(lo, mid, hi,ar[mid]))
+    if(target>ar[-1]):
+        return len(ar)
+    elif(target<=ar[0]):
+        return 0
+    else:
+        return mid+1
+
 def halfsearch_d(ar, target):
     lo=0
     hi=len(ar)-1 
@@ -57,9 +79,15 @@ def demo(bisect_fn):
         offset = position*"  |"
         print(ROW_FMT.format(needle, position, offset))
 
+def grade(bisect_fn, score, breakpoints =[60, 70, 80, 90], grades='FDCBA'):
+    i = bisect_fn(breakpoints, score)
+    return grades[i]
+
 if __name__ == '__main__':
     if sys.argv[-1] == 'left':
         bisect_fn = bisect.bisect_left
+    elif sys.argv[-1] == 'halfsearch_left':
+        bisect_fn = halfsearch_left
     elif sys.argv[-1] == 'halfsearch':
         bisect_fn = halfsearch
     elif sys.argv[-1] == 'halfsearch_d':
@@ -72,5 +100,13 @@ if __name__ == '__main__':
     print('index    ->', ' '.join('%2d' % n for n in range(len(HAYSTACK)+1)))
     print('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
     demo(bisect_fn)
+
+
+    scores = [33, 99, 77, 70, 89, 90, 100, 101, -1, 60]
+    print(scores)
+    print([grade(bisect_fn,score) for score in scores]) 
+
+
+
 
 
