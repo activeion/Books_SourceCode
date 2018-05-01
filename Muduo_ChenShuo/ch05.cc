b2c6730831d717d2b2bdd2b22f87092ca31c3849
activeion <ji.zhonghua@qq.com>
2018-05-02 06:45:21 +0800

    improve ch05.cc

--- Muduo_ChenShuo/ch05.cc
+++ Muduo_ChenShuo/ch05.cc
@@ -6,70 +6,36 @@
 #include <sys/resource.h>
 #include <unistd.h>
 
-
 #include <muduo/base/LogStream.h>
-
-
-//AsyncLogging
-off_t kRollSize = 500*1000*1000;
-muduo::AsyncLogging* g_asyncLog = NULL;
-void asyncOutput(const char* msg, int len)
-{
-    g_asyncLog->append(msg, len);
-}
-void bench(bool longLog)
-{//负责向缓冲区写入数据
-    muduo::Logger::setOutput(asyncOutput);
-
-    int cnt = 0;
-    const int kBatch = 1000;
-    muduo::string empty = " ";
-    muduo::string longStr(3000, 'X');
-    longStr += " ";
-
-    for (int t = 0; t < 30; ++t)
-    {
-        muduo::Timestamp start = muduo::Timestamp::now();
-        for (int i = 0; i < kBatch; ++i)
-        {
-            LOG_INFO << "Hello 0123456789" << " abcdefghijklmnopqrstuvwxyz "
-                << (longLog ? longStr : empty)
-                << cnt;
-            ++cnt;
-        }
-        muduo::Timestamp end = muduo::Timestamp::now();
-        printf("%f\n", timeDifference(end, start)*1000000/kBatch);
-        struct timespec ts = { 0, 500*1000*1000 };
-        nanosleep(&ts, NULL);
-    }
-}
 
 int main(int argc, char* argv[])
 {
-    
+
     {//Logging
-        LOG_INFO << "EventLoop created " << ;
+        LOG_INFO << "EventLoop created ";
         // 使用s+fin大发调试可以发现：
         // 秘密都藏在Logger类的析构函数中
         /*
-        Logger::~Logger()
-        {
-              impl_.finish();
-                const LogStream::Buffer& buf(stream().buffer());
-                  g_output(buf.data(), buf.length());
-                    if (impl_.level_ == FATAL)
-                          {
-                                  g_flush();
-                                      abort();
-                                        }
-        }
-        析构函数调用g_output()这个函数调用defaultOutput(),
-        将buf中的数据打印到屏幕！
-        Logger.stream_成员是一个LogStream类对象，LogStream类仅仅是一个提供了>>操作符的buffer!
-        */
+           Logger::~Logger()
+           {
+           impl_.finish();
+           const LogStream::Buffer& buf(stream().buffer());
+           g_output(buf.data(), buf.length());
+           if (impl_.level_ == FATAL)
+           {
+           g_flush();
+           abort();
+           }
+           }
+           析构函数调用g_output()这个函数调用defaultOutput(),
+           将buf中的数据打印到屏幕！
+           Logger.stream_成员是一个LogStream类对象，LogStream类仅仅是一个提供了>>操作符的buffer!
+           */
     }
 
     {//AsyncLogging
+        off_t kRollSize = 500*1000*1000;
+        static muduo::AsyncLogging* g_asyncLog = NULL;
         {
             // set max virtual memory to 2GB.
             size_t kOneGB = 1000*1024*1024;
@@ -86,6 +52,32 @@ int main(int argc, char* argv[])
         g_asyncLog = &log;
 
         bool longLog = argc > 1;
-        bench(longLog);
+        //bench(longLog);
+        //void bench(bool longLog)
+        {//负责向缓冲区写入数据
+            muduo::Logger::setOutput([](const char* msg, int len){g_asyncLog->append(msg, len);});
+
+            int cnt = 0;
+            const int kBatch = 1000;
+            muduo::string empty = " ";
+            muduo::string longStr(3000, 'X');
+            longStr += " ";
+
+            for (int t = 0; t < 30; ++t)
+            {
+                muduo::Timestamp start = muduo::Timestamp::now();
+                for (int i = 0; i < kBatch; ++i)
+                {
+                    LOG_INFO << "Hello 0123456789" << " abcdefghijklmnopqrstuvwxyz "
+                        << (longLog ? longStr : empty)
+                        << cnt;
+                    ++cnt;
+                }
+                muduo::Timestamp end = muduo::Timestamp::now();
+                printf("%f\n", timeDifference(end, start)*1000000/kBatch);
+                struct timespec ts = { 0, 500*1000*1000 };
+                nanosleep(&ts, NULL);
+            }
+        }
     }
 }
